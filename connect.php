8abfbe2aa5ec4602a99d4a2431595e0aee1ff62b
Raqibul Hasan Rahat <49594202+rahat7014@users.noreply.github.com>
2023-02-12 08:15:07 +0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php 
-$HOSTNAME = 'localhost';
-$USERNAME = 'root';
-$PASSWORD = '';
-$DATABASE = 'lighteniam';
+$HOSTNAME = 'us-cdbr-east-06.cleardb.net';
+$USERNAME = 'b7a817063f1e47';
+$PASSWORD = 'b4505738';
+$DATABASE = 'heroku_0ab9ae6cb687a6f';
 
 $con =mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABASE);
 
